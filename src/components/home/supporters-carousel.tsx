7ffe8d7136b92dc05b4928e2bd227761c25b947e
sergiohsantos <rm567254@fiap.com.br>
2026-07-03 15:56:02 -0300

    Randon Supporters

--- src/components/home/supporters-carousel.tsx
+++ src/components/home/supporters-carousel.tsx
@@ -4,16 +4,16 @@ import Autoplay from "embla-carousel-autoplay"
 
 const supporters = [
   { name: "Surya Dental", type: "mantenedor" },
+  { name: "Colgate", type: "apoiador" },
   { name: "Kess", type: "mantenedor" },
   { name: "3M", type: "apoiador" },
-  { name: "Colgate", type: "apoiador" },
-  { name: "Oral-B", type: "apoiador" },
-  { name: "Sensodyne", type: "apoiador" },
   { name: "Dentalclean", type: "apoiador" },
   { name: "Dental Cremer", type: "apoiador" },
   { name: "Henry Schein", type: "apoiador" },
+  { name: "Sensodyne", type: "apoiador" },
   { name: "Orthometric", type: "apoiador" },
   { name: "FGM", type: "apoiador" },
+  { name: "Oral-B", type: "apoiador" },
   { name: "Angelus", type: "apoiador" },
 ]
 
